4d73b818ed1fa9e140a0c6d2458ce6c2ef5401d5
ukkang <bownowbownow35@gmail.com>
2022-12-08 17:16:51 +0900

    first commit

---
+++ pages/moon/Crater.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Crater = () => {
+
+    return (
+        <>
+            <img src="https://www.shutterstock.com/image-photo/meteor-crater-natural-landmark-arizona-600w-1295315824.jpg" alt="no"/>
+            <h2>craters in the moon.</h2>
+        </>
+    );
+}
+export default Crater;
