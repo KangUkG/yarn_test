36dafe64914f3b908e381911486ce692636ec216
ukkang <bownowbownow35@gmail.com>
2022-12-08 18:00:13 +0900

    err build

--- pages/moon/Crater.js
+++ pages/moon/Crater.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const Crater = () => {
 
+    const a = undefined
     return (
         <>
             <img src="https://www.shutterstock.com/image-photo/meteor-crater-natural-landmark-arizona-600w-1295315824.jpg" alt="no"/>
             <h2>craters in the moon.</h2>
+            <h1>{a.go}</h1>
         </>
     );
 }
